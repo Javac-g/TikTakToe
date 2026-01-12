d8c3dde227ca6177bd3db74b781bc08914316958
Denysov Maksym <93475429+Javac-g@users.noreply.github.com>
2026-01-12 15:11:13 +0200

    Create Player.java

---
+++ Player.java
@@ -0,0 +1,14 @@
+public class Player {
+
+    private int sum;
+
+    public Player(){
+        sum = 0;
+    }
+    public int getSum(){
+        return sum;
+    }
+    public void setSum(int x){
+        sum += x;
+    }
+}
